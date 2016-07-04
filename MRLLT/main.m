fc29a93da00efd2c70b28b3b14b62202c066a808
alenQingshan <liuqingshan@liuqingshandemac-mini.local>
2016-07-04 13:17:25 +0800

    Initial Commit

---
+++ MRLLT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MRLLT
+//
+//  Created by 刘青山 on 16/7/4.
+//  Copyright © 2016年 刘青山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
